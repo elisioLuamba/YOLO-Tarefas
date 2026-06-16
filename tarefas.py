bc073e1ced4c731ca64ca4595b721390a345a0db
MELLO <elimassaqui@gmail.com>
2026-06-16 14:46:12 +0100

    SEGEMTACAO PERFETIA COM TELA BOA

--- tarefas.py
+++ tarefas.py
@@ -13,12 +13,12 @@ cores_por_id = {}
 # 2. PESO DAS CORES AJUSTADO (Transparência da máscara)
 # 0.4 significa 40% da cor da máscara e 60% da imagem real da webcam.
 # Isso deixa a máscara suave e permite ver o celular/pessoa por trás.
-peso_cor = 0.4
+peso_cor = 0.3
 
 cap = cv2.VideoCapture(0)
 
 # Opcional: Forçar a webcam a capturar em maior resolução (Melhora a precisão)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1100)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 700)
 
 if not cap.isOpened():
